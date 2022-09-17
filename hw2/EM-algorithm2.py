beb8466c41050dfe5cbfa21d7079e901b2a38fb8
ChrisRawstone <dr.rawstone@outlook.com>
2022-09-17 02:26:29 -0400

    em2

--- hw2/EM-algorithm2.py
+++ hw2/EM-algorithm2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import optparse
+import random
 import sys
 from collections import defaultdict
 
@@ -9,11 +10,12 @@ optparser.add_option("-e", "--english", dest="english", default="e", help="Suffi
 optparser.add_option("-f", "--french", dest="french", default="f", help="Suffix of French filename (default=f)")
 optparser.add_option("-t", "--threshold", dest="threshold", default=0.5, type="float",
                      help="Threshold for aligning with Dice's coefficient (default=0.5)")
-optparser.add_option("-n", "--num_sentences", dest="num_sents", default=10000, type="int",
+optparser.add_option("-n", "--num_sentences", dest="num_sents", default=1000, type="int",
                      help="Number of sentences to use for training and alignment")
 (opts, _) = optparser.parse_args()
 f_data = "%s.%s" % (opts.train, opts.french)
 e_data = "%s.%s" % (opts.train, opts.english)
+
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))][:opts.num_sents]
 f_count = defaultdict(int)
 e_count = defaultdict(int)
@@ -21,11 +23,11 @@ fe_count = defaultdict(int)
 t_ef = defaultdict(float)
 
 for (n, (f, e)) in enumerate(bitext):
-    for f_i in f:
+    for f_i in set(f):
         f_count[f_i] += 1
-        for e_j in e:
+        for e_j in set(e):
             fe_count[(e_j, f_i)] += 1
-    for e_j in e:
+    for e_j in set(e):
         e_count[e_j] += 1
     if n % 500 == 0:
         sys.stderr.write(".")
@@ -46,42 +48,81 @@ num_converged = 0
 NumberOfCombinations = len(fe_count)
 
 for key in fe_count:
-    t_ef[key] = 1 / NumberOfCombinations
+    t_ef[key] = random.uniform(0, 1)
 
 s_total = {}
 prev = 0
 convergenceSum = 0
 numProbabilities = 0
 
-while not_converged:
-    num_converged = 0
-    count = defaultdict(float)
-    total = defaultdict(float)
 
-    for (n, (f, e)) in enumerate(bitext):
-        for e_j in e:
-            s_total[e_j] = 0
-            for f_i in f:
-                s_total[e_j] += t_ef[(e_j, f_i)]
-        for e_j in e:
-            for f_i in f:
-                count[(e_j, f_i)] += t_ef[(e_j, f_i)] / s_total[e_j]
-                total[f_i] += t_ef[(e_j, f_i)] / s_total[e_j]
-        for f_i in f:
-            for e_j in e:
-                prev = t_ef[(e_j, f_i)]
-                t_ef[(e_j, f_i)] = count[(e_j, f_i)] / total[f_i]
-                convergenceSum += abs(prev - t_ef[(e_j, f_i)])
-                numProbabilities += 1
-    temp = convergenceSum / numProbabilities
-    if (convergenceSum / numProbabilities) < 0.002:
-        not_converged = False
+
+# def q(j,i,l,m):
+#     if flag:
+#         return random.randint(10)
+#     return c3[(j,i,l,m)]/c4[(i,l,m)]
+
+
+q = dict()
+for (k, (f, e)) in enumerate(bitext):  # for k = 1..n
+    l = len(e)
+    m = len(f)
+    for i in range(0, m):
+        for j in range(0, l):
+            q[(j, i, l, m)] = random.uniform(0, 1)
+
+def delta(k,i,j,e,f):
+    l_k=len(e)
+    m_k=len(f)
+    numerator = q[(j,i,l_k,m_k)]*t_ef[(e[j],f[i])]
+    denominator = sum([(q[(j,i,l_k,m_k)]*t_ef[(e[j],f[i])]) for j in range(0,l_k)])
+    return numerator/denominator
+
+c3 = defaultdict(float)
+
+iterations = 0
+
+
+
+for s in range(5):
+
+    c1 = defaultdict(float)
+    c2 = defaultdict(float)
+    c3 = defaultdict(float)
+    c4 = defaultdict(float)
+
+    for (k, (f, e)) in enumerate(bitext):  # for k = 1..n
+        l_k = len(e)
+        m_k = len(f)
+        for j, e_j in enumerate(e): # for
+            for i,f_i in enumerate(f):
+
+                c1[(e_j, f_i)] += delta(k,i,j,e,f)
+                c2[f_i] += delta(k,i,j,e,f)
+                c3[(j, i, l_k, m_k)] += delta(k,i,j,e,f)
+                c4[(i, l_k, m_k)] += delta(k,i,j,e,f)
+
+    for (k, (f, e)) in enumerate(bitext):  # for k = 1..n
+        l_k = len(e)
+        m_k = len(f)
+        for j, e_j in enumerate(e): # for
+            for i, f_i in enumerate(f): # for j = 0..l
+                q[(j,i,l_k,m_k)] = c3[(j, i, l_k, m_k)] / c4[(i, l_k, m_k)]
+                t_ef[(e_j,f_i)] = c1[(e_j, f_i)]/c2[f_i]
+
+    # print(t_ef[("the", "les")])
 
 for (f, e) in bitext:
   for (i, f_i) in enumerate(f):
     for (j, e_j) in enumerate(e):
-      if t_ef[(e_j, f_i)] >= 0.4:
+      if t_ef[(e_j, f_i)] >= 0.5:
         sys.stdout.write("%i-%i " % (i,j))
   sys.stdout.write("\n")
+
+
+
+
+
+
 
 
